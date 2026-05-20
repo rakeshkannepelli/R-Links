0788c79fe23539554e26b6d958dee3fca081b81a
rakesh <rakeshkannepelly185@gmail.com>
2026-05-20 22:38:23 +0530

    added google auth ui

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,12 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+import { GoogleOAuthProvider } from '@react-oauth/google';
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <GoogleOAuthProvider clientId="865692260796-7mhoi18o9tivclje90gq3748778suoru.apps.googleusercontent.com">
+      <App />
+    </GoogleOAuthProvider>
   </React.StrictMode>,
 )
